84567e880067414dc286c2f7b4b26a47dab73402
Markus Kahl <machisuji@gmail.com>
2018-06-20 21:01:42 +0100

    use working dependency from jcenter

--- hello.sbt
+++ hello.sbt
@@ -1,12 +1,3 @@
-resolvers += "Radix Bintray Repo" at "https://dl.bintray.com/radixdlt/maven2"
+resolvers += "JCenter" at "https://jcenter.bintray.com"
 
-// doesn't work, hence the self-built JAR under ./lib
-libraryDependencies += "com.radixdlt" % "radixdlt-java" % "0.9.0"
-
-// radix dependencies for the self-built JAR
-libraryDependencies += "org.bouncycastle" % "bcprov-jdk15on" % "1.56"
-libraryDependencies += "com.google.code.gson" % "gson" % "2.8.2"
-libraryDependencies += "com.squareup.okhttp3" % "okhttp" % "3.10.0"
-libraryDependencies += "com.squareup.okhttp3" % "logging-interceptor" % "3.10.0"
-libraryDependencies += "io.reactivex.rxjava2" % "rxjava" % "2.1.14"
-libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.25"
+libraryDependencies += "com.radixdlt" % "radixdlt-java" % "0.9.1"
--- src/main/scala/machisuji/HelloRadix.scala
+++ src/main/scala/machisuji/HelloRadix.scala
@@ -5,11 +5,21 @@ import com.radixdlt.client.core.address._
 import com.radixdlt.client.core.identity._
 import com.radixdlt.client.messaging.RadixMessaging
 
-object HelloRadix extends App {
+class HelloRadix {
   val universe = RadixUniverse.bootstrap(Bootstrap.SUNSTONE)
   val identity = new SimpleRadixIdentity()
   val addr = RadixAddress.fromString("JGuwJVu7REeqQtx7736GB9AJ91z5xB55t8NvteaoC25AumYovjp") // radix faucet
-  val msg = RadixMessaging.getInstance.sendMessage("Hello Faucet, this is Markus!", identity, addr)
 
-  msg.subscribe(s => println("RadixMessaging: " + s))
+  def msg = RadixMessaging.getInstance.sendMessage("Hello Faucet!", identity, addr)
+
+  def hello(): Unit = {
+    println("Sending hello to " + addr)
+    msg.subscribe(s => println("RadixMessaging: " + s))
+  }
+}
+
+object HelloRadix extends App {
+  val hr = new HelloRadix
+
+  hr.hello()
 }
