78931e3c66f30db2096bf96a606e5784df51b98b
Markus Kahl <machisuji@gmail.com>
2018-06-22 00:44:23 +0100

    print own address

--- src/main/scala/machisuji/HelloRadix.scala
+++ src/main/scala/machisuji/HelloRadix.scala
@@ -10,11 +10,15 @@ class HelloRadix {
   val identity = new SimpleRadixIdentity()
   val addr = RadixAddress.fromString("JGuwJVu7REeqQtx7736GB9AJ91z5xB55t8NvteaoC25AumYovjp") // radix faucet
 
-  def msg = RadixMessaging.getInstance.sendMessage("Hello Faucet, this is Markus!", identity, addr)
+  def msg = RadixMessaging.getInstance.sendMessage("Hello Faucet, this is me!", identity, addr)
+
+  def myAddress = universe.getAddressFrom(identity.getPublicKey)
 
   def hello(): Unit = {
-    println("Sending hello to " + addr)
-    msg.subscribe(s => println("RadixMessaging: " + s))
+    universe.getNetwork.connectAndGetStatusUpdates().subscribe(e => println(s"STATUS: $e"))
+
+    println(s"Sending hello to $addr from $myAddress")
+    msg.subscribe(e => println(s"MESSAGE: $e"))
 
     msg.doFinally(() => {
       println("Message sent. Shutting down.")
