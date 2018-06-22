9311d6fdb23f9a1c18c7062b3caba368a6605138
Markus Kahl <machisuji@gmail.com>
2018-06-22 01:09:17 +0100

    print balance

--- src/main/scala/machisuji/HelloRadix.scala
+++ src/main/scala/machisuji/HelloRadix.scala
@@ -1,9 +1,11 @@
 package machisuji
 
+import com.radixdlt.client.assets.Asset
 import com.radixdlt.client.core._
 import com.radixdlt.client.core.address._
 import com.radixdlt.client.core.identity._
 import com.radixdlt.client.messaging.RadixMessaging
+import com.radixdlt.client.wallet.RadixWallet
 
 class HelloRadix {
   val universe = RadixUniverse.bootstrap(Bootstrap.SUNSTONE)
@@ -14,10 +16,20 @@ class HelloRadix {
 
   def myAddress = universe.getAddressFrom(identity.getPublicKey)
 
+  def wallet: RadixWallet = RadixWallet.getInstance()
+  def balance: Double = wallet
+    .getXRDSubUnitBalance(myAddress)
+    .map[Double](balance => balance.toLong / Asset.XRD.getSubUnits)
+    .blockingFirst()
+
   def hello(): Unit = {
     universe.getNetwork.connectAndGetStatusUpdates().subscribe(e => println(s"STATUS: $e"))
 
-    println(s"Sending hello to $addr from $myAddress")
+    println("Address: " + myAddress)
+    println("Balance: " + balance)
+    println()
+
+    println(s"Sending hello to $addr")
     msg.subscribe(e => println(s"MESSAGE: $e"))
 
     msg.doFinally(() => {
